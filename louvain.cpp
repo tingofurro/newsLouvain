8d3b8148e4467bc62d819cee79bf96bf830b37df
IceMupppet <zhanghongjian.1995@gmail.com>
2017-04-19 18:26:21 -0700

    merge graph

--- louvain.cpp
+++ louvain.cpp
@@ -1,5 +1,4 @@
 #include "louvain.h"
-
 std::set<int> neighborCommunities(node_t* node, int* comm) {
 	vector<edge_t> neighbors = node->neighbors;
 	std::set<int> comm_set;
@@ -8,4 +7,47 @@ std::set<int> neighborCommunities(node_t* node, int* comm) {
 	}
 
 	return comm_set;
+}
+
+
+graph_t* mergeGraph(graph_t* graph, int* comm) {
+	graph_t* newGraph = (graph_t*) malloc(sizeof(graph_t));
+	std::set<int> comm_set;
+	for (int i = 0; i < graph->size; ++i) {
+		comm_set.insert(comm[i]);
+	}
+	newGraph->size = comm_set.size();
+	newGraph->nodes = (node_t*) malloc(comm_set.size() * sizeof(node_t));
+	printf("comm_set size: %d\n", newGraph->size);
+	for (int i = 0; i < graph->size; ++i) {
+		int cur_comm = comm[i];
+		std::vector<edge_t> cur_neighbors = newGraph->nodes[cur_comm].neighbors;
+		std::vector<edge_t> old_neighbors = graph->nodes[i].neighbors;
+		
+		std::vector<int> cur_neighbors_nodes;
+		for (int j = 0; j < cur_neighbors.size(); j++) {
+			cur_neighbors_nodes.push_back(cur_neighbors[j].target);
+		}
+
+		for(int j = 0; j < old_neighbors.size(); ++j) {
+			int old_neighbor_comm = comm[old_neighbors[j].target];
+			int exist = 0;
+			for (int k = 0; k < cur_neighbors_nodes.size(); ++k) {
+				if (cur_neighbors_nodes[k] == old_neighbor_comm) {
+					cur_neighbors[k].weight += old_neighbors[j].weight;
+					exist = 1;
+				}
+			}
+			// edge does not yet exist
+			if (exist == 0) {
+				edge_t *edgePtr = (edge_t*) malloc(sizeof(edge_t));
+				edgePtr->target = old_neighbor_comm; edgePtr->weight = old_neighbors[j].weight; 
+				cur_neighbors.push_back(*edgePtr);
+				newGraph->nodes[i].degree++;
+				printf("need to push %d to %d's neighbor list\n", old_neighbor_comm, cur_comm); 
+
+			}
+		}
+	}
+	return newGraph;
 }
--- serial.cpp
+++ serial.cpp
@@ -11,18 +11,19 @@ int main(int argc, char **argv) {
 	int comm[] = {0,6,1,1,2,2,3,3, 4, 5, 6, 7, 8};
 
 	std::set<int> comm_set = neighborCommunities(&graph->nodes[0], comm);
+	graph_t* newGraph = mergeGraph(graph, comm);
 
 	while(!comm_set.empty()) {
 		printf("%d\n", *comm_set.begin());
 		comm_set.erase(comm_set.begin());
 	}
 
-	// for (int i = 0; i < graph->size; i++) {
-	// 	vector<edge_t> neighbors = graph->nodes[i].neighbors;
-	// 	printf("Degree of node[%d] = %d\n", i, graph->nodes[i].degree);
-	// 	for(int j = 0; j < neighbors.size(); ++j) {
-	// 		printf("%d -> %d\n", i, neighbors[j].target);
-	// 	}
-	// }
+	for (int i = 0; i < newGraph->size; i++) {
+		vector<edge_t> neighbors = newGraph->nodes[i].neighbors;
+		printf("Degree of node[%d] = %d\n", i, newGraph->nodes[i].degree);
+		for(int j = 0; j < neighbors.size(); ++j) {
+			printf("%d -> %d\n", i, neighbors[j].target);
+		}
+	}
 	return 1;
 }
